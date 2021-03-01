c408caaa08ede83273d06292309820556d4689f2
Surfincolin <colinwageman@gmail.com>
2021-02-28 16:45:19 -0800

    Modularized sampler

--- heybrain/Sampler.py
+++ heybrain/Sampler.py
@@ -1,51 +1,78 @@
 import time
 import numpy as np
 
-import threading
+# import threading
 
 import brainflow
-from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds, LogLevels
-from brainflow.data_filter import DataFilter, FilterTypes
+from brainflow.board_shim import BoardShim, BrainFlowInputParams, BoardIds
+# from brainflow.data_filter import DataFilter, FilterTypes
 
 # Channel Info
-# eeg chan ['Fp1', 'Fp2', 'C3', 'C4', 'P7', 'P8', 'O1', 'O2']
-# eeg ch   [1, 2, 3, 4, 5, 6, 7, 8]
-# board acc ch [9, 10, 11]
-# other ch [12, 13, 14, 15, 16, 17, 18]
-# anolog ch [19, 20, 21]
-# sample rate 250
 # package ch 0
+# eeg chan assoc. ['Fp1', 'Fp2', 'C3', 'C4', 'P7', 'P8', 'O1', 'O2']
+# eeg ch  [1, 2, 3, 4, 5, 6, 7, 8]
+# board acc ch [9, 10, 11]
+# empty on cyton
+# other ch [12, 13, 14, 15, 16, 17, 18] 
+# anolog ch [19, 20, 21]
+
 # timestamp ch 22
 # marker ch 23
 
-def main():
+# sample rate 250
 
-  params = BrainFlowInputParams()
-  params.serial_port = '/dev/cu.usbserial-DM0258I7'
+class Sampler:
+  def __init__(self, port='/dev/cu.usbserial-DM0258I7', debug=False):
+    self.streaming = False
 
-  log = False
-  if log:
-    BoardShim.enable_dev_board_logger()
-  else:
-    BoardShim.disable_board_logger()
+    self.data_pool = np.zeros(shape=(24, 1024), dtype=float)
+
+    self.params = BrainFlowInputParams()
+    self.params.serial_port = port
+
+    log = debug
+    if log:
+      BoardShim.enable_dev_board_logger()
+    else:
+      BoardShim.disable_board_logger()
+
+    self.board = BoardShim(BoardIds.CYTON_BOARD, self.params)
 
 
-  board = BoardShim(BoardIds.CYTON_BOARD, params)
-  board.prepare_session()
-  board.start_stream(48000)
-  print('sleep...')
-  time.sleep(1)
-  data = board.get_board_data()  # get all data and remove it from internal buffer
-  
-interations = 0
-while interations < 10:
-  print(data.shape)
-  time.sleep(0.1)
-  data = board.get_board_data()
-  interations += 1
-  
-  board.stop_stream()
-  board.release_session()
+  def start_stream(self):
+    self.board.prepare_session()
+    self.board.start_stream(5120)
+    time.sleep(0.5)
+    self.streaming = True
+    print('::: Streaming :::')
 
+  def stop_stream(self):
+    self.streaming = False
+    self.board.stop_stream()
+    self.board.release_session()
+    print('::: Stream Stopped :::')
+
+  def get_data(self):
+    return self.board.get_board_data()
+
+  def mark_event(self, event_id=0):
+    self.board.insert_marker(event_id)
+
+
+
+# For testing purposes
 if __name__ == '__main__':
-  main()
+  sampler = Sampler()
+  sampler.start_stream()
+
+  iterations = 0
+  while iterations < 20:
+    print(sampler.get_data()[8])
+    time.sleep(0.1)
+    if iterations == 10:
+      sampler.mark_event(2)
+
+    iterations += 1
+
+  sampler.stop_stream()
+
